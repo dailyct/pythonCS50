d2df47b1b0e7e99a83a2890d1c84d3971b68f9a2
王孟帆 <myallmorris@gmail.com>
2025-07-06 14:04:47 +0800

    Add examples for basic Python data types
    Added four new scripts demonstrating usage of boolean, float, int, and string data types in Python. Each script includes variable assignments and print statements to illustrate type conversion and output.

---
+++ week0/Data Type/DataType of int.py
@@ -0,0 +1,13 @@
+print("Data Type")
+print("int:")
+#claim an x in integer 
+x = int(5)
+#output x
+print("x = 5")
+print("The number of int(x) is",x)
+
+#claim a y in integer 
+y = int(3.8)
+#output y ,the result should be 3
+print("y = 3.8")
+print("The number of int(y) is",y)
---
+++ week0/Data Type/DataType of string.py
@@ -0,0 +1,13 @@
+print("Data Type")
+print("string:")
+#claim an x in string 
+x = str(5)
+#output x
+print("x = str(5)")
+print("The string(x) is",x)
+
+#claim a y in string
+y = str("Hello, World!!")
+#output y 
+print("y = str("")")
+print("y is",y)
